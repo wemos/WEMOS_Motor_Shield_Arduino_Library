18ba8a5e64efdfc038463152173863aff6d78d3f
Thomas O Fredericks <tof@t-o-f.info>
2018-10-17 00:40:54 -0400

    Update examples/wemos_motor_shield_demo/wemos_motor_shield_demo.ino

--- examples/wemos_motor_shield_demo/wemos_motor_shield_demo.ino
+++ examples/wemos_motor_shield_demo/wemos_motor_shield_demo.ino
@@ -1,52 +1,66 @@
+#include <Wire.h>
+
+
 #include "WEMOS_Motor.h"
 
-int pwm;
 
-//Motor shiled I2C Address: 0x30
+
+//Motor shield default I2C Address: 0x30
 //PWM frequency: 1000Hz(1kHz)
-Motor M1(0x30,_MOTOR_A, 1000);//Motor A
-Motor M2(0x30,_MOTOR_B, 1000);//Motor B
+Motor M1(0x30, _MOTOR_A, 1000); //Motor A
+Motor M2(0x30, _MOTOR_B, 1000); //Motor B
 
 
 void setup() {
-  Serial.begin(250000);
+  
+  
+  Serial.begin(57600);
+  Serial.println("Starting demo");
+
+  
 }
 
 void loop() {
 
+  int pwm;
+
+  Serial.println("for (pwm = 0; pwm <= 100; pwm++) { M1.setmotor( _CW, pwm); M2.setmotor(_CW, 100-pwm); }");
   for (pwm = 0; pwm <= 100; pwm++)
   {
     M1.setmotor( _CW, pwm);
-    M2.setmotor(_CW, 100-pwm);
-    Serial.printf("A:%d%, B:%d%, DIR:CW\r\n", pwm,100-pwm);
+    M2.setmotor(_CW, 100 - pwm);
+    delay(100);
   }
-  
+
+  Serial.println("Motor STOP");
   M1.setmotor(_STOP);
   M2.setmotor( _STOP);
-  Serial.println("Motor A&B STOP");
-  delay(200);
-  
-  for (pwm = 0; pwm <=100; pwm++)
+
+  delay(1000);
+
+  Serial.println("for (pwm = 0; pwm <= 100; pwm++) { M1.setmotor(_CCW, pwm); M2.setmotor(_CCW, 100-pwm); }");
+  for (pwm = 0; pwm <= 100; pwm++)
   {
     M1.setmotor(_CCW, pwm);
-    M2.setmotor(_CCW, 100-pwm);
-    Serial.printf("A:%d%, B:%d%, DIR:CCW\r\n", pwm,100-pwm);
+    M2.setmotor(_CCW, 100 - pwm);
+    delay(100);
 
   }
-  
+
+  Serial.println("Motor STOP");
   M1.setmotor(_STOP);
   M2.setmotor( _STOP);
-  delay(200);
-  Serial.println("Motor A&B STOP");
+  delay(1000);
 
+
+  Serial.println("Motor A&B SHORT BRAKE");
   M1.setmotor(_SHORT_BRAKE);
   M2.setmotor( _SHORT_BRAKE);
-  Serial.println("Motor A&B SHORT BRAKE");  
   delay(1000);
-  
-  M1.setmotor(_STANDBY);//Both Motor standby
-  //M2.setmotor( _STANDBY);
-  Serial.println("Motor A&B STANDBY");  
+
+  Serial.println("Motor A&B STANDBY");
+  M1.setmotor(_STANDBY);
+  M2.setmotor( _STANDBY);
   delay(1000);
-  
+
 }
